c6e15890d660399b429c5bb42e65f7f7acb21ade
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-08-01 13:41:58 +0530

    socket fix

--- Client/src/socket/socketio.js
+++ Client/src/socket/socketio.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://interview-app-server.vercel.app/");
+const socket = io("https://interview-app-server.vercel.app/", {
+  withCredentials: true,
+  transports: ["websocket"],
+});
 
 export default socket;
