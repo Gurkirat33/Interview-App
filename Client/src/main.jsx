413bd731247740ced44151df7e0069830cb49552
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-08-01 14:13:06 +0530

    fix

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-if (process.env.NODE_ENV === "production") disableReactDevTools();
+// if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
